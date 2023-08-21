ee15f5356dfe1ffffd7fbaecaece138b3e50a5ab
Dieter Flubacher <98175786+dflubacher@users.noreply.github.com>
2023-08-21 13:31:55 +0200

    Remove cloudsmith installation of balena etcher.

--- scripts/install_etcher.sh
+++ scripts/install_etcher.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # ============================================================================
 # balena etcher
 # ----------------------------------------------------------------------------
 # TODO: only works for amd64.
+# NOTE: balena etcher is no longer hosted on cloudsmith.io.
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -11,46 +12,18 @@ echo
 echo "# ######################################################################"
 echo "# ### Installing balena etcher                                       ###"
 echo "# ######################################################################"
-# https://www.omgubuntu.co.uk/2017/05/how-to-install-etcher-on-ubuntu
-# (retrieved 2022-03-07).
 
 echo
-echo "##### Install apt repository and signing key..."
-# TODO: apt-key doesn't have a `deprecated` note in the man page in
-#       Ubuntu 20.04. What is the right way to deal with keys?
+echo "##### Download package from github..."
+installer_url=$(curl -s https://api.github.com/repos/balena-io/etcher/releases/latest | grep browser_download_url | cut -d\" -f4 | egrep 'amd64+.deb$')
+installer_path="/tmp/$(basename ${installer_url})"
 
-# This key seems to be outdated.
-# gpg --no-default-keyring --keyring /tmp/balena-io.gpg --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+echo $installer_path
 
-# https://askubuntu.com/a/1307181
-# with key from here:
-# https://github.com/balena-io/etcher/issues/3084#issuecomment-834486482
-pushd /tmp
-curl -Lo /tmp/balena-io.key https://dl.cloudsmith.io/public/balena/etcher/gpg.70528471AFF9A051.key
-
-gpg --no-default-keyring --keyring /tmp/temp-keyring.gpg --import /tmp/balena-io.key
-gpg --no-default-keyring --keyring ./temp-keyring.gpg --export --output balena-io.gpg
-
-[ -d /usr/local/share/keyrings ] || sudo mkdir -p /usr/local/share/keyrings
-
-sudo mv /tmp/balena-io.gpg /usr/local/share/keyrings/
-sudo rm /tmp/temp-keyring.gpg
-
-echo
-echo "##### Update sources list for balena etcher..."
-sudo tee /etc/apt/sources.list.d/balena-etcher.list > /dev/null <<'EOF'
-deb [signed-by=/usr/local/share/keyrings/balena-io.gpg] https://dl.cloudsmith.io/public/balena/etcher/deb/ubuntu focal main
-
-deb-src [signed-by=/usr/local/share/keyrings/balena-io.gpg] https://dl.cloudsmith.io/public/balena/etcher/deb/ubuntu focal main
-EOF
+curl -Lo ${installer_path} ${installer_url}
 
 echo
 echo "##### Installing balena etcher..."
-sudo apt-get update && sudo apt-get -y install balena-etcher-electron
+sudo apt-get update && sudo apt-get -y install ${installer_path}
 
-popd
 echo "##### Done!"
-
-# Script found here:
-# https://forums.balena.io/t/balenaetcher-repository-not-found/308436
-# curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' | sudo -E bash
